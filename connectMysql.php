e97ac7331b765e5a06d3d842862f9d3239b5fe89
sheway <67420772+sheway@users.noreply.github.com>
2020-06-25 23:09:24 +0800

    :bug:
    db_name

--- connectMysql.php
+++ connectMysql.php
@@ -2,7 +2,7 @@
 	$db_host = "localhost";
 	$db_username = "board";
 	$db_password = "1111";
-	$db_name = "board";
+	$db_name = "class_manager";
 	$db_link = @new mysqli($db_host, $db_username, $db_password, $db_name);
 	if($db_link -> connect_error != ""){
 		echo "connect fail!";

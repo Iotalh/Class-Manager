3ec47c7a75fb13fabc3f873da02ab6d520d0cb9c
sheway <67420772+sheway@users.noreply.github.com>
2020-06-27 02:50:58 +0800

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,9 +1,13 @@
 <?
 	session_start();
-	session_destroy();
-	$message = "Logout successful, have a nice day!";
-	
-	echo "<script>alert('$message'); 
-	location.href = 'index.php';</script>";
+	if(isset($_SESSION["userName"]) != NULL){
+		session_destroy();
+		$message = "Logout successful, have a nice day!";
+	}else{
+		echo "You havn't login yet, please try again!";
+		$message = "You havn't login yet, please try again!";
+	}
+	echo "<script>alert('$message');</script>";
+	echo "<script>location.href = 'index.php';</script>";
 
 ?>

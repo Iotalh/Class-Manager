dfccd00cf8eb12d63304e3c3c656d11241836842
sheway <67420772+sheway@users.noreply.github.com>
2020-06-27 02:51:58 +0800

    Update logout.php

--- logout.php
+++ logout.php
@@ -3,11 +3,10 @@
 	if(isset($_SESSION["userName"]) != NULL){
 		session_destroy();
 		$message = "Logout successful, have a nice day!";
+		echo "<script>alert('$message');</script>";
 	}else{
 		echo "You havn't login yet, please try again!";
-		$message = "You havn't login yet, please try again!";
 	}
-	echo "<script>alert('$message');</script>";
 	echo "<script>location.href = 'index.php';</script>";
 
 ?>

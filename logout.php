f8a101b19da9629a607a4f8358ff681519de2be5
sheway <67420772+sheway@users.noreply.github.com>
2020-06-27 02:26:50 +0800

    :tada: logout success

--- logout.php
+++ logout.php
@@ -1,15 +1,9 @@
 <?
 	session_start();
-	if(!isset($_SESSION["userName"]) || ($_SESSION["userName"] =="")){
-		header("Location: index.php");
-	}
-	if(isset($GET["logout"]) && ($_GET["logout"] == "true")){
-		unset($_SESSION["userName"]);
-		unset($_SESSION["userRole"]);
-		unset($_SESSION["studentId"]);
-		unset($_SESSION["department"]);
-		echo"<script>alert('logout success');
-		//location.herf='board.php';</script";
-	}
+	session_destroy();
+	$message = "Logout successful, have a nice day!";
+	
+	echo "<script>alert('$message'); 
+	location.href = 'index.php';</script>";
 
 ?>

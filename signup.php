bc5b0c883255efabf8a1034c8ee8e76d90a4a75c
sheway <67420772+sheway@users.noreply.github.com>
2020-06-26 23:09:08 +0800

    ::bug:: select

--- signup.php
+++ signup.php
@@ -1,143 +1,69 @@
 <?
-<<<<<<< Updated upstream
-=======
 function getSQLValue($value, $type)
 {
-	switch ($type) {
-		case "string":
-			$value = ($value != "") ? filter_var($value, FILTER_SANITIZE_MAGIC_QUOTES) : "";
-			break;
-		case "int":
-			$value = ($value != "") ? filter_var($value,FILTER_SANITIZE_NUMBER_INT) : "";
-			break;
-		case "email":
-			$value = ($value != "") ? filter_var($value, FILTER_VALIDATE_EMAIL) : "";
-			break;
-		case "url":
-			$value = ($value != "") ? filter_var($value, FILTER_VALIDATE_URL) : "";
-			break;
-	}
-	return $value;
+switch ($type) {
+case "string":
+$value = ($value != "") ? filter_var(
+$value,
+FILTER_SANITIZE_MAGIC_QUOTES
+) : "";
+break;
+case "int":
+$value = ($value != "") ? filter_var(
+$value,
+FILTER_SANITIZE_NUMBER_INT
+) : "";
+break;
+case "email":
+$value = ($value != "") ? filter_var(
+$value,
+FILTER_VALIDATE_EMAIL
+) : "";
+break;
+case "url":
+$value = ($value != "") ? filter_var(
+$value,
+FILTER_VALIDATE_URL
+) : "";
+break;
+}
+return $value;
 }
 
->>>>>>> Stashed changes
 if(isset($_POST['submit_info'])){
 	
-	$post_role = $_POST['role'];
-	$post_studentId = $_POST['studentId'];
-	$post_name = $_POST['name'];
-	$post_department = $_POST['department'];
-
 	if(isset($_POST["passwd"]) && isset($_POST["passwd_check"]) && ($_POST["passwd"] == $_POST["passwd_check"])){
 		require_once("connectMysql.php");
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-        //$sql_insert = "INSERT INTO account (Id ,role, studentId ,hashValue ,name ,department) VALUES (?, ?, ?, ?, ?, ?)";
-=======
-        $sql_insert = "INSERT INTO account (id ,role, studentId ,hashValue ,name ,department) VALUES (?, ?, ?, ?, ?, ?)";
->>>>>>> Stashed changes
-=======
-  // taskA_first_change
-        //$sql_insert = "INSERT INTO account (id ,role, studentId ,hashValue ,name ,department) VALUES (?, ?, ?, ?, ?, ?)";
->>>>>>> f216190c8ec21fa84eabf87ae09552fe307a5e46
+        $sql_insert = "INSERT INTO account (Id ,role, studentId ,hashValue ,name ,department) VALUES (?, ?, ?, ?, ?, ?)";
 		$stmt = $db_link->prepare($sql_insert);
 
-		$get_id_num_sql = "SELECT * FROM account ORDER BY id DESC LIMIT 0 , 1";
+		$get_id_num_sql = "SELECT * FROM account ORDER BY Id DESC LIMIT 0 , 1";
 		$all_id_num = $db_link->prepare($get_id_num_sql);
 		$id_num = $all_id_num -> num_rows;
 		$id_num = $id_num + 1;
+		echo "id_num= ". $id_num . "<br>";
 		$cal_hashValue = password_hash($_POST["passwd"], PASSWORD_BCRYPT);
-<<<<<<< Updated upstream
 		echo "cal_hashValue= ". $cal_hashValue . "<br>";
-
-		echo "data= ".$_POST["role"].$_POST["studentId"].$cal_hashValue. $_POST["name"].$_POST["department"];
-        $sql_insert = "INSERT INTO account (Id ,role, studentId ,hashValue ,name ,department) VALUES ($id_num, 
-		$_POST["role"], 
-		$_POST["studentId"], 
-		$cal_hashValue, 
-		$_POST["name"], 
-		$_POST["department"]);
-
-		$result = mysql_query($sql_insert, $stmt) or die(mysql_error());
-=======
 		
-
-		echo "status: num= ".$id_num." role= ".$post_role." studentID= ".$post_studentId." hashValue= ".$cal_hashValue." name= ".
-		$post_name." department= ".$post_department."<br>";
-
-
         $stmt->bind_param("iiissi",	$id_num,
 			getSQLValue($_POST["role"], "integer"),
 			getSQLValue($_POST["studentId"], "integer"),
 			$cal_hashValue, 
 			getSQLValue($_POST["name"], "string"),
 			getSQLValue($_POST["department"], "integer"));
->>>>>>> Stashed changes
 
 		$stmt->execute();
 		$stmt->close();
-		$db_link->close();
+		$db_link->close();//重新導向回到主畫面
 
 
 		$message="註冊成功";
-		echo "<script>alert('$message'); 
-		//location.href='login.php';</script>";
+		echo "<script>alert('$message'); location.href='login.php';</script>";
 
 	}
 	else{
 		$message="輸入的兩次密碼不符，請重新輸入";
 		echo "<script>alert('$message'); </script>";
-<<<<<<< HEAD
-=======
-		$sql_query = "SELECT * FROM account";
-		$result = $db_link->query($sql_query);
-		$row_result=$result->fetch_assoc();
-		$username = $row_result["Id"];
-		$hashValue = $row_result["hashValue"];
-        $db_link->close();
-        $password_check = password_verify ( $_POST["passwd"] ,  $hashValue);
-		if(($username==$_POST["username"]) && ($password_check == 'true')){
-			$_SESSION["loginMember"]=$username;
-			$message="登入成功";
-			echo "<script>alert('$message'); location.href = 'board.php';</script>";
-		}else if($username!=$_POST["username"]){
-			$message="username error, please relogin";
-			echo "<script>alert('$message'); location.href = 'login.php';</script>";
-		}else if($passwd!=$_POST["passwd"]){
-			$message="password error, please relogin";
-			echo "<script>alert('$message'); location.href = 'login.php';</script>";
-		}
-	}
-	else{
-		$message="輸入的兩次密碼不符，請重新輸入";
-		echo "<script>alert('$message'); location.href = 'login.php';</script>";
->>>>>>> master
-=======
-// master
-// 		$sql_query = "SELECT * FROM account";
-// 		$result = $db_link->query($sql_query);
-// 		$row_result=$result->fetch_assoc();
-// 		$username = $row_result["Id"];
-// 		$hashValue = $row_result["hashValue"];
-//         $db_link->close();
-//         $password_check = password_verify ( $_POST["passwd"] ,  $hashValue);
-// 		if(($username==$_POST["username"]) && ($password_check == 'true')){
-// 			$_SESSION["loginMember"]=$username;
-// 			$message="登入成功";
-// 			echo "<script>alert('$message'); location.href = 'board.php';</script>";
-// 		}else if($username!=$_POST["username"]){
-// 			$message="username error, please relogin";
-// 			echo "<script>alert('$message'); location.href = 'login.php';</script>";
-// 		}else if($passwd!=$_POST["passwd"]){
-// 			$message="password error, please relogin";
-// 			echo "<script>alert('$message'); location.href = 'login.php';</script>";
-// 		}
-// 	}
-// 	else{
-// 		$message="輸入的兩次密碼不符，請重新輸入";
-// 		echo "<script>alert('$message'); location.href = 'login.php';</script>";
->>>>>>> f216190c8ec21fa84eabf87ae09552fe307a5e46
 	}
 }
 ?>
@@ -171,20 +97,10 @@ if(isset($_POST['submit_info'])){
 					<li class="nav-item active">
 						<a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
 					</li>
-<<<<<<< HEAD
 
-<<<<<<< HEAD
-			<div class="input-group mb-3">
-  				<div class="input-group-prepend">
-			    	<span class="input-group-text" id="basic-addon1">姓名</span>
-				</div>
- 				<input type="text" class="form-control" placeholder="Enter Your Name" aria-label="Username" aria-describedby="basic-addon1"name="name" id="name" required>
-=======
 				</ul>
->>>>>>> master
-=======
+
 				</ul>
->>>>>>> f216190c8ec21fa84eabf87ae09552fe307a5e46
 			</div>
 		</nav>
 	</header>
@@ -194,15 +110,8 @@ if(isset($_POST['submit_info'])){
 				<div class="col-sm">
 					<h1 class="font-weight-bolder text-center">註冊</h1>
 				</div>
-<<<<<<< HEAD
-<<<<<<< HEAD
- 				<input type="text" class="form-control" placeholder="Enter Your Name" aria-label="Username" aria-describedby="basic-addon1"name="studentId" id="studentId" required>
-=======
->>>>>>> master
-=======
->>>>>>> f216190c8ec21fa84eabf87ae09552fe307a5e46
 			</div>
-			<form method="POST" name="formPost">
+			<form method="POST" name="formPost" action="" onSubmit="return checkForm();">
 				<div class="form-group row justify-content-md-center">
 					<div class="col-8">
 						<div class="input-group mb-3">
@@ -215,27 +124,7 @@ if(isset($_POST['submit_info'])){
 					</div>
 				</div>
 
-<<<<<<< HEAD
 
-<<<<<<< HEAD
-
-			<div class="row">
-				<legend class="col-form-label col-sm-2 pt-e">role</legend>
-				<div class="col-auto">
-					<div class="form-check">
-						<input class="form-check-input" type="radio" name="boardsex" id="0" value="admin" checked>
-						<label class="form-check-label" for="boardsex1">
-							Teacher
-						</label>
-					</div>
-					<div class="form-check">
-						<input class="form-check-input" type="radio" name="boardsex" id="1" value="student">
-						<label class="form-check-label" for="boardsex2">
-							Student
-						</label>
-=======
-=======
->>>>>>> f216190c8ec21fa84eabf87ae09552fe307a5e46
 				<div class="form-group row justify-content-md-center">
 					<div class="col-8">
 						<div class="input-group mb-3">
@@ -253,78 +142,30 @@ if(isset($_POST['submit_info'])){
 							<div class="input-group-prepend">
 								<label class="input-group-text" for="inputGroupSelect01">Role</label>
 							</div>
-							<select name="role" class="custom-select" id="role" >
+							<select class="custom-select" id="department">
 								<option value="" selected disabled hidden></option>
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-								<option name="boardsex" id="admin" value="admin">Teacher</option>
-								<option name="boardsex" id="student" value="student">Student</option>
-=======
-								<option value="1" id="admin">Teacher</option>
-								<option value="2" id="student">Student</option>
->>>>>>> Stashed changes
-							</select>
-						</div>
->>>>>>> master
-					</div>
-				</div>
-
-<<<<<<< HEAD
-
-			<div class="input-group mb-3">
-  				<div class="input-group-prepend">
-   					<label class="input-group-text" for="inputGroupSelect01">Department</label>
-  				</div>
-  				<select class="custom-select" id="department" required>
-				  	<option value="" selected disabled hidden>Department</option>
-    				<option value="0">資傳系</option>
-    				<option value="1">資工系</option>
-    				<option value="2">資訊英專</option>
-  				</select>
-			</div>
-
-			<div class="input-group mb-3">
-  				<div class="input-group-prepend">
-			    	<span class="input-group-text" id="basic-addon1">Password</span>
-=======
-=======
 								<option name="boardsex" id="admin" value="0">Teacher</option>
 								<option name="boardsex" id="student" value="1">Student</option>
 							</select>
 						</div>
 					</div>
 				</div>
->>>>>>> f216190c8ec21fa84eabf87ae09552fe307a5e46
+
 				<div class="form-group row justify-content-md-center">
 					<div class="col-8">
 						<div class="input-group mb-3">
 							<div class="input-group-prepend">
 								<label class="input-group-text" for="inputGroupSelect01">Department</label>
 							</div>
-							<select name = "department" class="custom-select" id= "department">
+							<select class="custom-select" id="department">
 								<option value="" selected disabled hidden></option>
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-								<option value="資傳系">資傳系</option>
-								<option value="資工系">資工系</option>
-								<option value="資英系">資英系</option>
-=======
-								<option value="1">資傳系</option>
-								<option value="2">資工系</option>
-								<option value="3">資英系</option>
->>>>>>> Stashed changes
-							</select>
-						</div>
-					</div>
->>>>>>> master
-=======
 								<option value="0">資傳系</option>
 								<option value="1">資工系</option>
 								<option value="2">資英系</option>
 							</select>
 						</div>
 					</div>
->>>>>>> f216190c8ec21fa84eabf87ae09552fe307a5e46
+					
 				</div>
 				<div class="form-group row justify-content-md-center">
 					<div class="col-8">

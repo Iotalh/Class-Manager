bd68093c46002cc9bd96322b296d2014a912c377
Iotalh <marceline.cheng@gmail.com>
2020-06-28 21:27:54 +0800

    💄 Improve UI

--- signup.php
+++ signup.php
@@ -59,6 +59,7 @@ if (isset($_POST['submit_info'])) {
 	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.js"></script>
 	<link rel="stylesheet" href="css/nav.css">
 	<title>註冊</title>
 </head>
@@ -153,3 +154,25 @@ if (isset($_POST['submit_info'])) {
 		</div>
 	</div>
 </body>
+<script>
+	var $inputCheck = $(".form-control");
+	var $selectCheck = $(".custom-select");
+	$inputCheck.focusout(function() {
+		if ($(this).val() != "") {
+			$(this).removeClass("is-invalid");
+			$(this).addClass("is-valid");
+		} else {
+			$(this).addClass("is-invalid");
+			$(this).removeClass("is-valid");
+		}
+	});
+	$selectCheck.focusout(function() {
+		if ($(this).val() != null) {
+			$(this).removeClass("is-invalid");
+			$(this).addClass("is-valid");
+		} else {
+			$(this).addClass("is-invalid");
+			$(this).removeClass("is-valid");
+		}
+	});
+</script>
